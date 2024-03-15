07cb079e7d7ed55ee2df83146be0868a32c1ffab
Andre Lobo <loboandre@hotmail.com>
2024-03-15 15:23:07 -0400

    feat: Refactor emailService to use SendGrid API

--- service/emailService.js
+++ service/emailService.js
@@ -1,60 +1,53 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config(); // Carregar variáveis de ambiente do arquivo .env
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const EmailService = {
+  // Função para enviar e-mails de boas-vindas
   async sendWelcomeEmail(userEmail, userName) {
-    try {
-      let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS
-        }
-      });
+    if (!userEmail || !userName) {
+      throw new Error('userEmail e userName são parâmetros necessários');
+    }
 
-      let mailOptions = {
-        from: process.env.SMTP_USER,
+    try {
+      const msg = {
         to: userEmail,
+        from: 'xonga73@gmail.com', // substitua pelo seu e-mail verificado no SendGrid
         subject: 'Bem-vindo ao nosso aplicativo!',
         text: `Olá ${userName},\n\nBem-vindo ao nosso aplicativo! Esperamos que você aproveite sua experiência.\n\nAtenciosamente,\nEquipe do Aplicativo`
       };
 
-      await transporter.sendMail(mailOptions);
-
+      await sgMail.send(msg);
       console.log('Email de boas-vindas enviado com sucesso!');
     } catch (error) {
       console.error('Erro ao enviar o e-mail de boas-vindas:', error);
+      throw error;
     }
   },
 
+  // Função para enviar e-mails de redefinição de senha
   async sendPasswordResetEmail(userEmail, resetToken) {
-    try {
-      let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS
-        }
-      });
+    if (!userEmail || !resetToken) {
+      throw new Error('userEmail e resetToken são parâmetros necessários');
+    }
 
-      let mailOptions = {
-        from: process.env.SMTP_USER,
+    try {
+      const msg = {
         to: userEmail,
+        from: 'xonga73@gmail.com', // substitua pelo seu e-mail verificado no SendGrid
         subject: 'Redefinição de senha',
         text: `Olá,\n\nVocê solicitou a redefinição de senha. Clique no link abaixo para redefinir sua senha:\n\nhttp://localhost:7777/reset-password?token=${resetToken}\n\nSe você não solicitou esta redefinição, ignore este e-mail.\n\nAtenciosamente,\nEquipe do Aplicativo`
       };
 
-      await transporter.sendMail(mailOptions);
-
+      await sgMail.send(msg);
       console.log('Email de redefinição de senha enviado com sucesso!');
     } catch (error) {
       console.error('Erro ao enviar o e-mail de redefinição de senha:', error);
+      throw error;
     }
   }
 };
 
 module.exports = EmailService;
+
